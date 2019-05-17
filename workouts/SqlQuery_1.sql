498b7cf05b35a3a26da2b3c775b627f484b6f0fe
D.Naga Chandana <D.Naga.Chandana@dxc.com>
2019-05-17 15:41:31 +0530

    edmx file created

--- workouts/SqlQuery_1.sql
+++ workouts/SqlQuery_1.sql
@@ -1,24 +1,23 @@
-﻿create database workouttrackerproject
-use workouttrackerproject
-create table workout_category
+﻿create database workouttrackerprojects
+use workouttrackerprojects
+create table workout_categorys
 (
 category_id int primary key identity,
 category_name varchar(64),
 
-);
-create table workout_collection(
+)
+create table workout_collections(
 workout_title varchar(128),
 workout_note varchar(256),
 calories_burn_per_minute float,
-category_id int foreign key references workout_category(category_id), 
+category_id int foreign key references workout_categorys(category_id), 
 workout_id int  identity primary key
-);
+)
 
 
-
-create table Workout_Activess(
+create table Workout_Activss(
 id int identity primary key,
-workout_id int foreign key references workout_collection(workout_id),
+workout_id int foreign key references workout_collections(workout_id),
 start_time time,
 starts_date date,
 end_date date,
